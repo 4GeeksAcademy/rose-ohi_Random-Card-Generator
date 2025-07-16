697f5d8b7ffed2e734a74556b362c893bf84a73a
Roseanna O. <purposely.us@gmail.com>
2025-07-16 21:22:35 +0000

    DonneeE

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => { /* NOTE: ARROW functions are best used when using classes or REACT */
+   //write your code here 
+   document.querySelector('.card').classList.add(generateRandSuite());
+   document.querySelector('.card').innerHTML = (generateRandNum());
+
 };
+
+let generateRandNum = () =>{
+        let cente = ["Q","2","3","4","5","6","7","8","9","10","A","J","K"];
+        let numIndex = Math.floor(Math.random() * cente.length);
+
+        return cente[numIndex];
+    };
+
+let generateRandSuite = () => {
+        let suits = ["diamonds", "spades","clubs", "hearts", "hearts2", "diamonds2"];
+        let suitsIndex = Math.floor(Math.random() * suits.length);
+
+        return suits[suitsIndex];
+    };
